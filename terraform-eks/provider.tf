c262953182aac1480fb51d2bee44c3fe9741d98c
ahmetbekan <ahmet.bekenov@gmail.com>
2021-01-09 02:19:23 +0000

    vault added

--- terraform-eks/provider.tf
+++ terraform-eks/provider.tf
@@ -5,3 +5,15 @@ provider "aws" {
 # terraform {
 #     version = "0.12.19"
 # }
+
+# Unseal Key 1: 23OhFt5YYEhT12Yun9NiWRqWREonsfWDjlfPOB4h414g
+# Unseal Key 2: 475j9W79/aOgeqEDgvu3/EcGjwRW14+NEozomvkVZOho
+# Unseal Key 3: PvxeQC+jFxwxOv4Q+gp0HanlO2HS7kYhAaY5J+Ptb8Rv
+# Unseal Key 4: WKjH5XjfkoHIY6M8hsvjKzb4w1knyPrB64QO4jHtGUTP
+# Unseal Key 5: GRYkxSAT2f/SHq5V7niH0uAN+LtMFSqLQM32xP0qtsVz
+
+#Initial Root Token: s.vf6TFYaIuyCXFCFR4u8A9dqU
+
+# kubectl create secret generic eks-creds \
+#     --from-literal=AWS_ACCESS_KEY_ID="${AKIA4LXKSW4BQUCNOIRL}" \
+#     --from-literal=AWS_SECRET_ACCESS_KEY="${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}"
